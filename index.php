187885e0967509c932b0162af9609e301d41272b
mxrkuz <markuskoenig.8772@gmail.com>
2021-06-09 18:31:05 +0200

    Updatet Createpage
    UwU

--- index.php
+++ index.php
@@ -4,21 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="Login_CSS.css">
+    <link rel="stylesheet" type="text/css" href="Website/Createpage/Login_CSS.css">
 </head>
 <body>
     <div>
-      
+
     <form class="login" type = "post" >
-      <p class="title">Log in</p>
-      <input type= "text" placeholder= "Termin erstellen" name = "nickname" autofocus/>
-       <input class="button">   
+      <p class="title">Create Session</p>
+      <input type= "text" placeholder= "Topic" name = "topic" autofocus/>
+      <input type= "text" placeholder= "Nickname" name = "nickname" autofocus/>
+       <button class="button">Create</button>
     </form>
-
-    <php>
-
-    </php>
-
   </footer>
     </p>
   </div>
