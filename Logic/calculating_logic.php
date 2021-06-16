095975ca921b9d4ada3788ae7c8e7c86942ed0fb
MaverickTFMB <80470124+MaverickTFMB@users.noreply.github.com>
2021-06-16 10:29:31 +0200

    logic refactors

--- Logic/calculating_logic.php
+++ Logic/calculating_logic.php
@@ -1,32 +1,13 @@
 <?php
+include_once 'utils_database.php';
 print("php datei verbunden");
-
+echo("php datei verbunden");
 if(isset($_POST["nickname"])||isset($_POST["sessionName"])){
    // global $nickname = $_POST["nickname"];
    // global $nickname = $_POST["sessionName"];
     
 }
+$statement = "INSERT INTO Sessions (Name,Nickname) VALUES ('Englisch-lern','Mave')";
+saveToDatabase($statement);
 
-
-
-function saveToDatabase(){
-    connectToDatabase();
-}
-
-function connectToDatabase(){
-    $connection = mysqli_connect("localhost","Data-Admin","%Hgfr13");
-    $db = mysqli_select_db($connection,'Session-Database');
-    if (!$connection) {
-        echo('Verbindung schlug fehl:');
-    }else{
-        echo 'Erfolgreich verbunden';
-    }
-   
-    $sql_statement = "INSERT INTO Sessions (Name,Nickname) VALUES ('Englisch-lern','Mave')";
-    if(!mysqli_query($connection,$sql_statement)){
-        echo("Inserted");
-    }
-    mysqli_close($connection);
-    echo"Connection established and closed";
-}
 ?>
---
+++ Logic/utils_database.php
@@ -0,0 +1,21 @@
+<?php
+function connectToDatabase(){
+    $connection = mysqli_connect("localhost","Data-Admin","%Hgfr13");
+    $db = mysqli_select_db($connection,'Session-Database');
+    if (!$connection) {
+        echo('Verbindung schlug fehl:');
+    }else{
+        echo 'Erfolgreich verbunden';
+    }
+   
+    $sql_statement = "INSERT INTO Sessions (Name,Nickname) VALUES ('Englisch-lern','Mave')";
+    if(!mysqli_query($connection,$sql_statement)){
+        echo("Inserted");
+    }
+    mysqli_close($connection);
+    echo"Connection established and closed";
+}
+
+function saveToDatabase(){
+    connectToDatabase();
+}
