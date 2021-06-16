7bf8f848fddd62556057e05e47f4f71be509a9f0
MaverickTFMB <80470124+MaverickTFMB@users.noreply.github.com>
2021-06-16 10:19:51 +0200

    first test in sql

--- Logic/calculating_logic.php
+++ Logic/calculating_logic.php
@@ -2,8 +2,8 @@
 print("php datei verbunden");
 
 if(isset($_POST["nickname"])||isset($_POST["sessionName"])){
-    global $nickname = $_POST["nickname"];
-    global $nickname = $_POST["sessionName"];
+   // global $nickname = $_POST["nickname"];
+   // global $nickname = $_POST["sessionName"];
     
 }
 
@@ -17,9 +17,15 @@ function connectToDatabase(){
     $connection = mysqli_connect("localhost","Data-Admin","%Hgfr13");
     $db = mysqli_select_db($connection,'Session-Database');
     if (!$connection) {
-        die('Verbindung schlug fehl:');
+        echo('Verbindung schlug fehl:');
+    }else{
+        echo 'Erfolgreich verbunden';
     }
-    echo 'Erfolgreich verbunden';
+   
+    $sql_statement = "INSERT INTO Sessions (Name,Nickname) VALUES ('Englisch-lern','Mave')";
+    if(!mysqli_query($connection,$sql_statement)){
+        echo("Inserted");
+    }
     mysqli_close($connection);
     echo"Connection established and closed";
 }
