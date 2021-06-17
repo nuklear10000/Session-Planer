c0e836e49b6c1bae41fbd8aeac4deac0a7a04c5d
MaverickTFMB <80470124+MaverickTFMB@users.noreply.github.com>
2021-06-17 09:02:47 +0200

    Update index_insert.php

--- Logic/index_insert.php
+++ Logic/index_insert.php
@@ -10,7 +10,7 @@ if(isset($_POST["nickname"])&&isset($_POST["sessionName"])){
 }
 $nickname = $_POST["nickname"];
    $sessionName = $_POST["sessionName"];
-   $statement = "INSERT INTO Sessions (Name,Nickname) VALUES ($sessionName,$nickname)";
+   $statement = "INSERT INTO Sessions (Name,Nickname) VALUES (\"$sessionName\",\"$nickname\")";
    saveToDatabase($statement);
    echo "everything executed";
 
