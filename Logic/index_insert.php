f0a8249c71413ec70453eb132337f5d924a9cd92
MaverickTFMB <80470124+MaverickTFMB@users.noreply.github.com>
2021-06-17 09:25:21 +0200

    Update index_insert.php

--- Logic/index_insert.php
+++ Logic/index_insert.php
@@ -1,16 +1,11 @@
 <?php
 include_once 'utils_database.php';
-//echo("php datei verbunden");
 if(isset($_POST["nickname"])&&isset($_POST["sessionName"])){
-   $nickname = $_POST["nickname"];
-   //$sessionName = $_POST["sessionName"];
-   $statement = "INSERT INTO Sessions (Name,Nickname) VALUES ($sessionName,$nickname)";
-   saveToDatabase($statement);
-   echo "everything executed";
 }
 
    $nickname = $_POST["nickname"];  
    $sessionName = $_POST["sessionName"];
+   print($sessionName);
    $statement = "INSERT INTO Sessions (Name,Nickname) VALUES (\"$sessionName\",\"$nickname\")";
    print_r($_POST);
    print "<br>";
