5adfd8251b3bf6a7d287af0f9ec29785e66aab8d
nuklear10000 <56518296+nuklear10000@users.noreply.github.com>
2021-06-28 22:35:45 +0200

    Update script.js

--- Website/Main Table/script.js
+++ Website/Main Table/script.js
@@ -66,13 +66,8 @@ const renderCalendar = () => {
     ) {
       days += `<div class="today">${i}</div>`;
     } else {
-
       days += `<div id="myElement${i}">${i}</div>`;
     }
-
-      days += `<div>${i}</div>`;
-    } 
-
   }
 
   for (let j = 1; j <= nextDays; j++) {
@@ -80,105 +75,6 @@ const renderCalendar = () => {
     monthDays.innerHTML = days;
   }
 };
-
-function myFunction() {
-  var MyDiv1 = document.getElementById('DIV1');
-    if(MyDiv1.innerHTML.isS("<div>1</div>")) {
-      alert("true");
-    } else {
-      alert("false");
-    }
-    /* if(MyDiv1.innerHTML.includes("2")) {             change line 67 with  days += `<div>${i}</div>`; "tipp"
-      alert("2");
-    } 
-    if(MyDiv1.innerHTML.includes("3")) {
-      alert("3");
-    } 
-    if(MyDiv1.innerHTML.includes("4")) {
-      alert("4");
-    }  
-    if(MyDiv1.innerHTML.includes("5")) {
-      alert("5");
-    } 
-    if(MyDiv1.innerHTML.includes("6")) {
-      alert("6");
-    } 
-    if(MyDiv1.innerHTML.includes("7")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("8")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("9")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("10")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("11")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("12")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("13")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("14")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("15")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("16")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("17")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("18")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("19")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("20")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("21")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("22")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("23")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("24")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("25")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("26")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("27")) {
-      alert("4");
-    } 
-    if(MyDiv1.innerHTML.includes("28")) {
-      alert("28");
-    } 
-    if(MyDiv1.innerHTML.includes("29")) {
-      alert("29");
-    } 
-    if(MyDiv1.innerHTML.includes("30")) {
-      alert("30");
-    } 
-    if(MyDiv1.innerHTML.includes("31")) {
-      alert("31");
-    } */
-}
 
 document.querySelector(".prev").addEventListener("click", () => {
   date.setMonth(date.getMonth() - 1);
@@ -189,6 +85,7 @@ document.querySelector(".next").addEventListener("click", () => {
   date.setMonth(date.getMonth() + 1);
   renderCalendar();
 });
+
 renderCalendar();
 
   var myElement1 = document.getElementById("myElement1");
@@ -356,5 +253,3 @@ renderCalendar();
       alert(myElement31.innerHTML);
     });
   }
-
-renderCalendar();
